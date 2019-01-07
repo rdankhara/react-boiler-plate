3a91b7df02e3ee4d81c23fb1691d058291c46cda
Rajnikant Dankhara <rajdankh@publicisgroupe.net>
2019-01-07 19:31:48 +0000

    added @babel/plugin-proposal-properties so that react state synctex can be processed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
 class App extends React.Component{
+    constructor(){
+        super();
+        this.state = { 
+            count : 0
+        };
+
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+    }
+    state = {
+        count : 0
+    }
+
+    increment(){
+        this.setState({count: this.state.count + 1});
+    }
+
+    decrement(){
+        this.setState({count : this.state.count -1});
+    }
     render() {
-        debugger;
-        return <h1>Hello World</h1>;
+        return (
+            <React.Fragment>
+                <h1>Hello World</h1>
+                <h2>Count: {this.state.count}</h2>
+                <button onClick={ this.increment }>+</button>
+                <button onClick={ this.decrement }>-</button>
+            </React.Fragment>
+        
+        );
     }
 }
 
